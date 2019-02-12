4589cd5748c6560476f6f1e732bee0a47fd32c49
Chris Armitage <dev@chrisarmitage.com>
2019-02-12 15:20:35 +0000

    Add RestActionRouter (/resource/1/actions/complete)

---
+++ src/Framework/Router/RestActionRouter.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Framework\Router;
+
+class RestActionRouter
+{
+    protected $controllerNamespace;
+
+    protected $methodActions = [
+        'GET' => 'Index',
+        'POST' => 'Create',
+        'PUT' => 'Update',
+        'DELETE' => 'Delete',
+    ];
+
+    /**
+     * @param string|null $controllerNamespace
+     */
+    public function __construct(?string $controllerNamespace)
+    {
+        $this->controllerNamespace = $controllerNamespace ?? 'Application\\Controller\\';
+    }
+
+    /**
+     * @param string $url
+     * @param string $method
+     * @return RestRoute
+     */
+    public function getRouteForUrl($url, $method = 'GET') : RestRoute
+    {
+        $url = parse_url($url);
+
+        preg_match_all("#/(?<resource>[\w\-]+)(?:/(?<id>(?!actions)[\w\-]+))?(?:/actions/(?<action>[\w\-]+))?#", $url['path'], $matches);
+
+        $resourceElements = array_map(
+            function($url) {
+                return str_replace(
+                    ' ',
+                    '',
+                    ucwords(
+                        str_replace(
+                            '-',
+                            ' ',
+                            strtolower($url)
+                        )
+                    )
+                );
+            },
+            $matches['resource']
+        );
+
+        $resourceName = $this->controllerNamespace . array_pop($resourceElements);
+        $controllerType = $this->methodActions[$method];
+        $resourceId = (\count($matches['resource']) === \count($matches['id']))
+            ? array_pop($matches['id'])
+            : null;
+
+        if ($controllerType === 'Index' && $resourceId !== '') {
+            $controllerType = 'Read';
+        }
+
+        if ($method === 'POST' && \count($matches['action']) >= 1) {
+            $action =  str_replace(
+                ' ',
+                '',
+                ucwords(
+                    str_replace(
+                        '-',
+                        ' ',
+                        strtolower(array_pop($matches['action']))
+                    )
+                )
+            );
+
+            $controllerType = 'Action\\' . $action;
+        }
+        $controllerName = $resourceName . '\\' . $controllerType;
+
+        $nestedResources = [];
+        foreach ($resourceElements as $key => $value) {
+            $nestedResources[$value] = $matches['id'][$key];
+        }
+
+        return new RestRoute($controllerName, $resourceId, $nestedResources);
+    }
+}
